d48ea583f2c7e3e6dfdccefbc1eb52e27e80c6e7
Matthew Cuyar <matt@enctypeapparel.com>
2014-07-26 00:41:26 -0400

    created a module container for usage throughout station

--- Station/Lib/station.rb
+++ Station/Lib/station.rb
@@ -1,5 +1,13 @@
 class Station
-    def Station.provision(config, settings)
+
+    attr_accessor :config, :settings
+
+    @@modules = Hash.new
+
+    def initialize(config, args)
+        @config = config
+        @settings = args
+
         # Get the modules directory
         @modules = Dir.glob($path + '/Station/modules/*').select {|f| File.directory? f}
 
@@ -12,17 +20,31 @@ class Station
             @classname = @args["classname"]
 
             # Merge settings with default config
-            if (settings.has_key?(@basename) && !settings[@basename].empty?)
-                @args = @args.deep_merge(settings[@basename])
+            if (@settings.has_key?(@basename) && !@settings[@basename].empty?)
+                @args = @args.deep_merge(@settings[@basename])
             end
 
             # run the module provisioner
             require m + "/#{@basename}.rb"
             m.sub! $path, '/vagrant'
             @class = Kernel.const_get(@classname).new(config, @args, m)
-            @class.provision
+            @@modules[@classname] = @class
 
         end
 
+    end
+
+    def modules
+        return @@modules
+    end
+
+    def module(classname)
+        return @@modules[classname]
+    end
+
+    def provision
+        @@modules.each do |name, object|
+            object.provision
+        end
     end
 end
--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,7 @@ require $path + '/bootstrap.rb'
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   Homestead.configure(config, $station_config)
-  Station.provision(config, $station_config)
+
+  $station = Station.new(config, $station_config)
+  $station.provision
 end
