ecf1cf864b3793f9d9930e9d2658641a94a178c7
Matthew Cuyar <matt@enctypeapparel.com>
2014-08-22 23:49:36 -0400

    added mysql module

---
+++ Station/modules/mysql/mysql.rb
@@ -0,0 +1,55 @@
+class Mysql < StationModule
+
+  def create_db(name, user, drop = true)
+
+    unless drop === true
+      drop = %{
+        sudo mysql -uroot -psecret #{name} -e "
+        DROP DATABASE IF EXISTS #{name};
+      "}
+
+      shell_provision(drop)
+    end
+
+    script = %{
+      sudo mysql -uroot -psecret #{name} -e "
+      CREATE DATABASE IF NOT EXISTS #{name};
+      GRANT ALL ON #{name} TO '#{user}'@'localhost';
+    "}
+
+    shell_provision(script)
+
+  end
+
+  def create_user(username, password)
+
+    script = %{
+      sudo mysql -uroot -psecret -e "
+        GRANT USAGE ON *.* TO '#{username}'@'localhost';
+        DROP USER '#{username}'@'localhost';
+        CREATE USER '#{username}'@'localhost' IDENTIFIED BY '#{password}';
+      "}
+
+    shell_provision(script)
+
+  end
+
+  def provision
+
+    # Install databases
+      args.find?('databases', []).each do |db|
+
+        #create user
+        if db.has_key?("user") && db.has_key?("password")
+          create_user(db["user"], db["password"])
+        end
+
+        if db.has_key?("name")
+          create_db(db["name"], db.find?('user', 'homestead'))
+        end
+
+      end
+
+  end
+
+end
