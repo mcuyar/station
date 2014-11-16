cba899a7debd598a692da1c8f454fab95e185218
Matthew Cuyar <matt@enctypeapparel.com>
2014-11-15 23:55:52 -0500

    updated station modules - added check allowing  module params to be a string, hash, or array - added classname propert to initialize method

--- Station/Lib/station-module.rb
+++ Station/Lib/station-module.rb
@@ -3,6 +3,7 @@ class StationModule
   attr_accessor :config, :args, :scripts
 
   def initialize(config, args, module_path)
+    @classname = args.is_a?(Hash) ? args.delete('classname') : self.class
     @config = config
     @args = args
     @scripts = module_path + "/scripts"
