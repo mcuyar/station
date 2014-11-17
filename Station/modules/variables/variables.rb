2ab6fa97f08d3d7d2607a02e0e5fcfcd6b8a5fc1
Matthew Cuyar <matt@enctypeapparel.com>
2014-11-17 00:09:24 -0500

    updated variables module

--- Station/modules/variables/variables.rb
+++ Station/modules/variables/variables.rb
@@ -1,16 +1,28 @@
 class Variables < StationModule
 
-  def env_var(key, value)
-    shell_provision(
-        "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf && service php5-fpm restart",
-        [key, value]
-    )
+  def env_var(key, value = nil)
+    if key.is_a?(Array)
+      key.each do |var|
+        shell_provision(
+            "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf",
+            [var["key"], var["value"]]
+        )
+      end
+    elsif key.is_a?(Hash)
+      key.each do |k,v|
+        shell_provision(
+            "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf",
+            [k, v]
+        )
+      end
+    end
+    shell_provision("service php5-fpm restart")
   end
 
   def provision
     # Configure All Of The Server Environment Variables
-    args.find?('variables', []).each do |var|
-      env_var(var["key"], var["value"])
+    if args.find?('variables', false)
+      env_var(args["variables"])
     end
   end
 
