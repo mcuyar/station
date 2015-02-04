18f9065b5e760d6250ad0101a44355f274ab4a47
Matthew Cuyar <matt@enctypeapparel.com>
2015-02-04 10:21:54 -0500

    added ssl cert options

---
+++ Station/modules/ssl/ssl.rb
@@ -0,0 +1,41 @@
+class SSL < StationModule
+
+  attr_accessor :ssl_config, :path
+
+  def initialize(config, args, module_path)
+    super
+    @ssl_config = {}
+    @path = "#{File.dirname(__FILE__)}"
+  end
+
+  def set_config
+    default = args["defaults"]["config"]
+    @ssl_config = default.merge(args.find?('config', {}))
+  end
+
+  def generate(domain, config)
+
+    set_config
+
+    config = @ssl_config.merge(config || {})
+    config["domain"] = domain
+
+    template = File.read(path + "/templates/subj.erb")
+    result = ERB.new(template, nil, '>').result(binding)
+
+    # Generate the SSL Cert
+    shell_provision(
+      "bash #{scripts}/generate.sh \"$1\" \"$2\"",
+      [domain, result]
+    )
+
+  end
+
+  def provision
+
+      # Create our SSL directory in case it doesn't exist
+      shell_provision('sudo mkdir -p "/etc/ssl"')
+
+  end
+
+end
